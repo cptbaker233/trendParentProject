5c5aec9b5f362b21a5b3ad2e6f209c9e38e60d2d
Bobo <cpt_hujinbo@foxmail.com>
2020-03-27 09:26:16 +0800

    新增日期区间勾选

--- trend-trading-backtest-service/src/main/java/com/hujinbo/trend/controller/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/com/hujinbo/trend/controller/BackTestController.java
@@ -1,5 +1,7 @@
 package com.hujinbo.trend.controller;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hujinbo.trend.pojo.IndexData;
 import com.hujinbo.trend.service.BackTestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,21 +10,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class BackTestController {
     @Autowired
     BackTestService backTestService;
 
-    @GetMapping("simulate/{code}")
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String, Object> backTest(@PathVariable("code") String code) throws Exception {
+    public Map<String, Object> backTest(@PathVariable("code") String code, @PathVariable("startDate") String strStartDate, @PathVariable("endDate") String strEndDate) throws Exception {
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
+        //获取第一天的日期和最后一天的日期
+        String indexStartDate = allIndexDatas.get(0).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size() - 1).getDate();
+        //过滤掉非区间数据
+        allIndexDatas = filterByDateRange(allIndexDatas, strStartDate, strEndDate);
+
         Map<String, Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
+        result.put("indexStartDate", indexStartDate);
+        result.put("indexEndDate", indexEndDate);
+        return result;
+    }
+
+    //根据日期过滤指数数据的方法
+    private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
+        //空值判断, 如果日期有空则不筛选返回全部
+        if (StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate)) {
+            return allIndexDatas;
+        }
+        List<IndexData> result = new ArrayList<>();
+        Date startDate = DateUtil.parse(strStartDate);
+        Date endDate = DateUtil.parse(strEndDate);
+        for (IndexData indexData : allIndexDatas) {
+            Date date = DateUtil.parse(indexData.getDate());
+            if (date.getTime() >= startDate.getTime() && date.getTime() <= endDate.getTime()) {
+                result.add(indexData);
+            }
+        }
         return result;
     }
 
